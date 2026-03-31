5c3e2e89589c4fb6b9309d849aee7e2af0a7eaf7
Shoe2 <andrewelee1983@gmail.com>
2026-03-30 20:06:49 -0700

    more q28

--- code/question_28.gd
+++ code/question_28.gd
@@ -1,21 +1,20 @@
 extends Node
 
+@onready var phoneReciever = $PhoneReciever
+@onready var phoneBase = $PhoneBase
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$FiftyFifty.pressed.connect(_fifty_pressed)
+	$PhoneAFriend.pressed.connect(_phone_a_friend_pressed)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func fifty_pressed():
+func _fifty_pressed():
 	$PhoneAFriend.hide()
 	$FiftyFifty.hide()
 	
-func phone_a_friend_pressed():
+func _phone_a_friend_pressed():
 	$PhoneAFriend.hide()
 	$FiftyFifty.hide()
-	# ANIMATE PHONE PUNCH
+	phoneReciever.show()
+	phoneBase.show()
+	
